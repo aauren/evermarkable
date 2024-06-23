ec13141987d7004f2e7d59c06ab794b7ef92a060
Aaron U'Ren <aauren@gmail.com>
2024-06-23 15:08:26 -0500

    fix(IsExpired): fall through to true to renew user token

--- pkg/remarkable/api/utils.go
+++ pkg/remarkable/api/utils.go
@@ -24,7 +24,7 @@ func (a *authToken) IsExpired() (bool, error) {
 		return false, nil
 	}
 
-	return false, nil
+	return true, nil
 }
 
 func getRemarkableConfigFromCtx(httpClientCtx *HTTPClientCtx) (*model.EMRemarkableConfig, error) {
